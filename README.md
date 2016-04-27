4aae4cff9c866b02c8475fd361ea7fc64304b02e
doublelam <1422015387@qq.com>
2016-04-27 21:40:37 +0800

    new frontending sample with typescript

---
+++ README.md
@@ -0,0 +1,16 @@
+#README.md
+create by lam
+
+这是一个模板  用于前端   
+
+#TypeScript
+使用typescript  当然你可以在js文件夹找到对应的js文件
+在dist文件夹会有压缩后的 .min.js文件
+
+
+#Less
+使用less  你不能找到 相应的css文件，但是在dist文件夹会有相应的压缩后的 .min.css文件
+
+
+
+
---
+++ src/scripts/test.js
@@ -0,0 +1,11 @@
+var getInter;
+getInter = function (a, b) {
+    var result = a.search(b);
+    return result !== -1 ? true : false;
+    // 
+};
+console.log(getInter('handsome', 'han'));
+var tt = function () {
+    console.log('dd');
+};
+tt();
