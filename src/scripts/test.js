9bd256b0968e41f3612c342cbc059c90b840bf0c
doublelam <1422015387@qq.com>
2016-04-28 20:47:11 +0800

    new changed

--- src/scripts/test.js
+++ src/scripts/test.js
@@ -1,6 +1,24 @@
-var getInter;
-getInter = function (a, b) {
-    var result = a.search(b);
-    return result !== -1 ? true : false;
-    // 
-};
+/**
+ * GetElement
+ */
+var GetElement = (function () {
+    function GetElement(eleName) {
+        var strToEleType = { '.': 'class', '#': 'id' };
+        var eleTypeDes = eleName.substr(0, 1);
+        this.element = document.createElement('div');
+    }
+    return GetElement;
+}());
+var getElement = new GetElement('s');
+console.log(getElement.element);
+// interface ShowWaterfall{
+// 	containerName: HTMLElement;
+// 	childNames: Array<HTMLElement>;
+// }
+// class WaterfallLayout implements ShowWaterfall {
+// 	containerName: HTMLElement;
+// 	childNames: Array<HTMLElement>;
+// 	constructor(){
+// 		this.containerName = document.getElememt
+// 	}
+// } 
--- src/typescripts/test.ts
+++ src/typescripts/test.ts
@@ -1,10 +1,34 @@
-interface testFunc{
-	(name: string,subname:string):boolean;
+interface FetchElement{
+	element: HTMLElement
 }
 
-var getInter:testFunc;
-getInter = function(a:string,b:string){
-	var result = a.search(b);
-	return result!==-1 ? true: false;
-	// 
+/**
+ * GetElement
+ */
+class GetElement implements FetchElement {
+	element: HTMLElement;
+	constructor(eleName: string) {
+		const strToEleType = {'.': 'class','#':'id'};
+		var eleTypeDes = eleName.substr(0,1);
+		this.element =  document.createElement('div');
+	}
 }
+
+const getElement = new GetElement('s');
+console.log(getElement.element);
+
+
+
+// interface ShowWaterfall{
+// 	containerName: HTMLElement;
+// 	childNames: Array<HTMLElement>;
+// }
+
+
+// class WaterfallLayout implements ShowWaterfall {
+// 	containerName: HTMLElement;
+// 	childNames: Array<HTMLElement>;
+// 	constructor(){
+// 		this.containerName = document.getElememt
+// 	}
+// }
